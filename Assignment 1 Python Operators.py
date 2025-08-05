78c1c7deda5b500f8e1e8cf4cefbbc4688babe28
zahoorali215-create <zahoorali215@gmail.com>
2025-08-05 22:28:54 +0500

    uploading assignments folder

---
+++ Assignment 1 Python Operators.py
@@ -0,0 +1,41 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=========================== RESTART: E:/Assignments/Assignment 1 Python Operators.py ===========================
+Q.1 Arithmetic Operators
+addition:a + z =  19
+Subtraction: a - b = 11
+Multiplication: a * b = 60
+Division: a / b = 3.75
+Floor Division: a // b = 3
+Modulus: a % b = 3
+Exponentiation: a ** b = 50625
+Q.2 Arithmetic Assignment Operators
+Initial value of b: 10
+After b += 10: 15
+After b -= 5: 13
+After b *= 8: 52
+After b /= 2: 26.0
+Q.3 Comparison Operators
+a == z: False
+a != z: True
+a > z: False
+a < z: True
+a >= z: False
+a <= z: True
+Q.4: Logical Operators
+a and z: False
+a or z: True
+not a: False
+Q.5 Membership Operators
+’s’ in institute: True
+'Saylani' in institute: True
+'Saylani' not in institute: False
+Q.6: Identity Operators
+a is b: True
+a is c: False
+c is not b: True
+BONUS Challange
+Enter your username: Talha
+Enter your password: Axiom123
+login successful!
