ac110bc08ce7a2482902c27c3f9166a9182907dd
zahoorali215-create <zahoorali215@gmail.com>
2025-08-15 20:05:57 +0500

    Delete Q.3 Expected Output (Answer).py.py

--- Q.3 Expected Output (Answer).py.py
+++
@@ -1,7 +0,0 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> 
-= RESTART: E:/Ai and Data Science/Assignment 2/Practicing Control-Flow (Decision-Making Structures and Loops)/Q.3 py/Python Code/code.py
-Enter a number: 2
-1 is Odd
-2 is Even
