711879217636bf8c8e7b7bd9fd44596a141048e2
zahoorali215-create <zahoorali215@gmail.com>
2025-08-15 20:05:34 +0500

    Delete Expected Output (Answer).1.py

--- Expected Output (Answer).1.py
+++
@@ -1,9 +0,0 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> 
-= RESTART: E:/Ai and Data Science/Assignment 2/Practicing Control-Flow (Decision-Making Structures and Loops)/Q.1 py/Python Code/code.py
-The number is positive.
-The number is false.
-Enter percentage: 88
-A grade
-End of code
